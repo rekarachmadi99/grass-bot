253b89761aa4386fe2e0cb983c09b416a4b27ed2
Reka Rachmadi Apriansyah <reka.rachmadi@gmail.com>
2024-11-19 09:40:13 +0700

    update bot grass

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require('colors');
-const inquirer = require('inquirer');
-const Bot = require('./src/Bot');
-const Config = require('./src/Config');
+require("colors");
+const inquirer = require("inquirer");
+const Bot = require("./src/Bot");
+const Config = require("./src/Config");
 const {
   fetchProxies,
   readLines,
   selectProxySource,
-} = require('./src/ProxyManager');
-const { delay, displayHeader } = require('./src/utils');
+} = require("./src/ProxyManager");
+const { delay, displayHeader } = require("./src/utils");
 
 async function main() {
   displayHeader();
@@ -18,43 +18,55 @@ async function main() {
   const config = new Config();
   const bot = new Bot(config);
 
-  const proxySource = await selectProxySource(inquirer);
+  const proxySource = await selectProxySource();
 
   let proxies = [];
-  if (proxySource.type === 'file') {
+  if (proxySource.type === "file") {
     proxies = await readLines(proxySource.source);
-  } else if (proxySource.type === 'url') {
+  } else if (proxySource.type === "url") {
     proxies = await fetchProxies(proxySource.source);
-  } else if (proxySource.type === 'none') {
-    console.log('No proxy selected. Connecting directly.'.cyan);
+  } else if (proxySource.type === "none") {
+    console.log("No proxy selected. Connecting directly.".cyan);
   }
 
-  if (proxySource.type !== 'none' && proxies.length === 0) {
-    console.error('No proxies found. Exiting...'.red);
+  if (proxySource.type !== "none" && proxies.length === 0) {
+    console.error("No proxies found. Exiting...".red);
     return;
   }
 
   console.log(
-    proxySource.type !== 'none'
+    proxySource.type !== "none"
       ? `Loaded ${proxies.length} proxies`.green
-      : 'Direct connection mode enabled.'.green
+      : "Direct connection mode enabled.".green
   );
 
-  const userIDs = await readLines('uid.txt');
+  const userIDs = await readLines("uid.txt");
   if (userIDs.length === 0) {
-    console.error('No user IDs found in uid.txt. Exiting...'.red);
+    console.error("No user IDs found in uid.txt. Exiting...".red);
     return;
   }
 
   console.log(`Loaded ${userIDs.length} user IDs\n`.green);
 
   const connectionPromises = userIDs.flatMap((userID) =>
-    proxySource.type !== 'none'
+    proxySource.type !== "none"
       ? proxies.map((proxy) => bot.connectToProxy(proxy, userID))
       : [bot.connectDirectly(userID)]
   );
-
   await Promise.all(connectionPromises);
 }
 
-main().catch(console.error);
+function startHourlyProcess() {
+  async function restartProcess() {
+    await main().catch(console.error);
+    console.log("\nProcess finished. Restarting in 1 hour...\n".yellow);
+
+    setTimeout(() => {
+      restartProcess();
+    }, 3600000);
+  }
+
+  restartProcess();
+}
+
+startHourlyProcess();
